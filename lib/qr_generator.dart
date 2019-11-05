479783c41e8e42e9506b33c1e22482f2566fb830
Madhukesh <madhukesh04@gmail.com>
2019-11-05 17:09:47 +0530

    added QR generator

--- lib/qr_generator.dart
+++ lib/qr_generator.dart
@@ -1,17 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter/rendering.dart';
 
-class QRGenerator extends StatelessWidget {
+class QRGenerator extends StatefulWidget {
   const QRGenerator({Key key}) : super(key: key);
 
   @override
+  _QRGeneratorState createState() => _QRGeneratorState();
+}
+
+class _QRGeneratorState extends State<QRGenerator> {
+  GlobalKey globalKey = GlobalKey();
+  String _dataString = "";
+  String _inputErrorText;
+  final TextEditingController _textController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('QR Generator'),
-      ),
-      body: Center(
-        child: Text('generate'),
+        appBar: AppBar(
+          title: Text('QR Generator'),
+        ),
+        body: _contentWidget());
+  }
+
+  _contentWidget() {
+    final bodyHeight = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).viewInsets.bottom;
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 30.0,
+              left: 20.0,
+              right: 10.0,
+              bottom: 20.0,
+            ),
+            child: Container(
+              height: 40.0,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        hintText: "Enter a custom message",
+                        errorText: _inputErrorText,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: FlatButton(
+                      color: Colors.amber,
+                      child: Text("SUBMIT"),
+                      onPressed: () {
+                        setState(() {
+                          _dataString = _textController.text;
+                          _inputErrorText = null;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: RepaintBoundary(
+                key: globalKey,
+                child: QrImage(
+                  data: _dataString,
+                  size: 0.5 * bodyHeight,
+                  onError: (ex) {
+                    print("[QR] ERROR - $ex");
+                    setState(() {
+                      _inputErrorText =
+                          "Error! Maybe your input value is too long?";
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
